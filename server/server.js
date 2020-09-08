d00249470136611f5b64fa6f30ba332f6ff73ea0
Michael070791 <alhassanmic18@Yahoo.com>
2020-09-08 07:37:41 -0700

    refactoring

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+//promise
 mongoose.Promise = global.Promise;
 const app = express();
 //connection to mongodb cloud database
